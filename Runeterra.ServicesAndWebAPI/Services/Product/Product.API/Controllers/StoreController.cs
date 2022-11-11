03fb4cc81d574f52f8f25cbb837fe4da4f65d37a
Trong Hoang <tronggaw2@gmail.com>
2022-11-11 10:12:32 +0700

    SH-18 User RESTful API

---
+++ Runeterra.ServicesAndWebAPI/Services/Product/Product.API/Controllers/StoreController.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Product.API.Entity;
+
+namespace Product.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StoreController : ControllerBase
+{
+    private readonly IRequestClient<ApplicationUser> _client;
+
+    public StoreController(IRequestClient<ApplicationUser> client)
+    {
+        _client = client;
+    }
+    
+    string baseUrl = "https://localhost:5001/";
+    
+    
+}
